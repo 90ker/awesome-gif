541b1d16697e39d50c0c63be5e8f8ed48ae289ec
90ker <z1582373436@gmail.com>
2023-04-11 16:43:57 +0800

    清除多余文件,引入test.gif

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <img alt="Vue logo" class="logo" src="./assets/test.gif" width="400" height="250" />
   </header>
 
   <main>
-    <TheWelcome />
   </main>
 </template>
 
