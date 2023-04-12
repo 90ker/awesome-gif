ffe24fe8763e07a74a57511f19458e50fb28195e
90ker <z1582373436@gmail.com>
2023-04-12 16:36:56 +0800

    引入gifuct解析出图片

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,50 @@
 <script setup>
+import { onMounted, reactive } from 'vue';
+import { parseGIF, decompressFrames } from 'gifuct-js';
+
+let gif_url = 'https://img.soogif.com/MEjPTY0DMzVvOnG4FDt0DVb7yGFsBJG3.gif';
+let gif = reactive({});
+let frames = reactive([]);
+onMounted(() => {
+  fetch(gif_url)
+    .then(res => res.arrayBuffer())
+    .then(buffer => {
+      gif = parseGIF(buffer);
+      frames = decompressFrames(gif, true);
+      console.log('gif', gif);
+      console.log('frames', frames);
+      const parsedImgWrapper = document.getElementById('piw');
+      for (let frame of frames) {
+        drawParsedImg(frame, parsedImgWrapper);
+      }
+    });
+});
+
+function drawParsedImg(frame, parsedImgWrapper) {
+  const dims = frame.dims;
+  const parsedImgCanvas = document.createElement('canvas');
+  parsedImgCanvas.style.marginLeft = '10px';
+  const parsedImgCtx = parsedImgCanvas.getContext('2d');
+  parsedImgCanvas.width = dims.width;
+  parsedImgCanvas.height = dims.height;
+
+  const frameImgData = parsedImgCtx.createImageData(dims.width, dims.height);
+  frameImgData.data.set(frame.patch);
+  parsedImgCtx.putImageData(frameImgData, 0, 0);
+
+  parsedImgWrapper.appendChild(parsedImgCanvas);
+}
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/test.gif" width="400" height="250" />
+    <img class="logo" src="https://img.soogif.com/MEjPTY0DMzVvOnG4FDt0DVb7yGFsBJG3.gif" width="400" height="250" />
   </header>
 
   <main>
+    <div id="piw" class="parsed-img-wrapper">
+    </div>
   </main>
 </template>
 
@@ -15,10 +53,14 @@ header {
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+
+.parsed-img-wrapper {
+  display: flex;
+  width: 900px;
+  flex-wrap: nowrap;
+  overflow-x: scroll;
 }
+
 
 @media (min-width: 1024px) {
   header {
